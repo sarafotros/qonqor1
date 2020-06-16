44c3c0edfeed2007afdf740eff5368cb1add3e3d
sarafotros <sarafotros@gmail.com>
2020-06-16 18:41:46 +0100

    tab navigate

--- App.tsx
+++ App.tsx
@@ -4,11 +4,13 @@ import { NavigationContainer } from '@react-navigation/native';
 // import { StyleSheet, Text, View } from 'react-native';
 
 import AuthNavigator from './navigation/AuthNavigator';
+import HomeNavigator from './navigation/HomeNavigator';
 
 export default function App() {
 	return (
 		<NavigationContainer>
-			<AuthNavigator />
+			{/* <AuthNavigator /> */}
+			<HomeNavigator />
 		</NavigationContainer>
 	);
 }
---
+++ navigation/HomeNavigator.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/home/Home';
+import Profile from '../screens/home/Profile';
+import Search from '../screens/home/Search';
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+
+export default function HomeNavigator() {
+	const Tab = createBottomTabNavigator();
+
+	return (
+		<Tab.Navigator
+			tabBarOptions={{
+				activeTintColor: 'tomato',
+				inactiveTintColor: 'gray',
+			}}
+		>
+			<Tab.Screen
+				options={{
+					tabBarIcon: ({ focused, color, size }) => (
+						<AntDesign
+							name="home"
+							size={focused ? 26 : 20}
+							color={focused ? 'tomato' : 'grey'}
+						/>
+					),
+				}}
+				name="Home"
+				component={Home}
+			/>
+			<Tab.Screen
+				options={{
+					tabBarIcon: ({ focused, color, size }) => (
+						<AntDesign
+							name="user"
+							size={focused ? 26 : 20}
+							color={focused ? 'tomato' : 'grey'}
+						/>
+					),
+				}}
+				name="Profile"
+				component={Profile}
+			/>
+			<Tab.Screen
+				options={{
+					tabBarIcon: ({ focused, color, size }) => (
+						<AntDesign
+							name="search1"
+							size={focused ? 26 : 20}
+							color={focused ? 'tomato' : 'grey'}
+						/>
+					),
+				}}
+				name="Search"
+				component={Search}
+			/>
+		</Tab.Navigator>
+	);
+}
+
+const styles = StyleSheet.create({});
